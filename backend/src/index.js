2b90e53f4782e6543b30f51dcfe08d29cce4f973
Tristan W <tristanwang99@yahoo.com>
2024-01-25 15:02:47 -0800

    Created register and login routes.

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,11 +1,13 @@
 import express from 'express'
 import cors from 'cors'
 import mongoose from 'mongoose'
+import { userRouter } from './routes/users.js'
 
 const app = express()
 
 app.use(express.json())
 app.use(cors())
+app.use("/auth", userRouter)
 
 mongoose.connect("mongodb+srv://aawang99:password12345@recipes.7bff0nu.mongodb.net/recipes?retryWrites=true&w=majority")
 
---
+++ backend/src/routes/users.js
@@ -0,0 +1,30 @@
+import express from 'express'
+import jwt from 'jsonwebtoken'
+import bcrypt from 'bcrypt'
+import { UserModel } from '../models/Users.js'
+
+// For login and registration.
+
+const router = express.Router()
+
+router.post("/register", async (req, res) => {
+  const { username, password } = req.body
+  const user = await UserModel.findOne({ username })
+  if (user) return res.status(400).json({ message: "User already exists." })
+  const hashedPassword = await bcrypt.hash(password, 10)
+  const newUser = new UserModel({ username, password: hashedPassword })
+  await newUser.save()
+  res.status(200).json({ message: "User registered." })
+})
+
+router.post("/login", async (req, res) => {
+  const { username, password } = req.body
+  const user = await UserModel.findOne({ username })
+  if (!user) return res.status(400).json({ messsage: "User doesn't exist." })
+  const isPasswordValid = await bcrypt.compare(password, user.password)
+  if (!isPasswordValid) return res.status(400).json({ message: "Username or password is incorrect." })
+  const token = jwt.sign({ id: user._id }, "secret")
+  res.status(200).json({ token, userId: user._id })
+})
+
+export { router as userRouter }
