58002dc0671810310e5daf058beac6eb4961c270
Tristan W <tristanwang99@yahoo.com>
2024-01-25 21:14:53 -0800

    Created saved recipes page.

--- frontend/src/pages/saved-recipes.js
+++ frontend/src/pages/saved-recipes.js
@@ -1,3 +1,42 @@
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+import { useGetUserId } from '../hooks/useGetUserId'
+
 export const SavedRecipes = () => {
-  return <div>Saved Recipes</div>
+  const [savedRecipes, setSavedRecipes] = useState([])
+  const userId = useGetUserId()
+
+  useEffect(() => {
+    const fetchSavedRecipes = async () => {
+      try {
+        const response = await axios.get(`http://localhost:5500/recipes/savedRecipes/${userId}`)
+        setSavedRecipes(response.data.savedRecipes)
+      } catch (err) {
+        console.error(err)
+      }
+    }
+
+    fetchSavedRecipes()
+  }, [])
+
+  return (
+    <div className="container">
+      <h2>Saved Recipes</h2>
+      <ul className='recipes-list'>
+        {savedRecipes.map((recipe) => (
+          <li key={recipe._id}>
+            {savedRecipes.includes(recipe._id) && <h3>Saved</h3>}
+            <div>
+              <h3>{recipe.name}</h3>
+            </div>
+            <div>
+              <p>{recipe.instructions}</p>
+            </div>
+            <img src={recipe.imageUrl} alt="" />
+            <p>Cooking Time: {recipe.cookingTime} minutes</p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  )
 }
